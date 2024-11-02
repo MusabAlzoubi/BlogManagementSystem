708bb143a12fd59138cff4f3d8dda59723a0f54b
MusabAlzoubi <musab.m.alzoubii@gmail.com>
2024-11-02 21:18:39 +0300

    Preparing UserResourse

---
+++ BlogManagementSystem/app/Filament/Resources/UserResource.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Forms\Form $form): Forms\Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->unique(ignoreRecord: true),
+                Forms\Components\TextInput::make('password')
+                    ->password() 
+                    ->required()
+                    ->minLength(8)
+                    ->dehydrateStateUsing(fn($state) => bcrypt($state))
+                    ->hiddenOn('edit'),
+                Forms\Components\Select::make('roles')
+                    ->relationship('roles', 'name')
+                    ->multiple()
+                    ->preload(),
+            ]);
+    }
+
+    public static function table(Tables\Table $table): Tables\Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('email')->sortable()->searchable(),
+                Tables\Columns\TextColumn::make('roles.name')->label('Roles')->sortable(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
--- BlogManagementSystem/routes/web.php
+++ BlogManagementSystem/routes/web.php
@@ -6,7 +6,7 @@ use Filament\Http\Livewire\Auth\Login;
 Route::middleware('auth')->group(function () {
 });
 
-Route::get('/admin', [Login::class, 'render'])->name('admin.login');
+// Route::get('/admin', [Login::class, 'render'])->name('admin.login');
 
 Route::get('/', function () {
     return view('welcome');
